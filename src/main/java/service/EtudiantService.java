05df397da2b737dcba4ed549ddf5ea373fe15fe3
Manos2605 <sonwadimitry26@gmail.com>
2026-01-13 11:10:57 +0100

    fin

--- src/main/java/service/EtudiantService.java
+++ src/main/java/service/EtudiantService.java
@@ -1,37 +1,39 @@
-package main.java.service;
-import java.util.ArrayList;
-import main.java.model.Etudiant;
+    package main.java.service;
+    import java.util.ArrayList;
+    import main.java.model.Etudiant;
 
-public class EtudiantService {
-    
-    ArrayList<Etudiant> etudiants = new ArrayList<>();
+    public class EtudiantService {
+        
+        ArrayList<Etudiant> etudiants = new ArrayList<>();
 
-    public void ajouter(Etudiant etudiant) {
-        etudiants.add(etudiant);
-    }
-
-    public void supprimer(Etudiant etudiant) throws Exception {
-        if (!etudiants.remove(etudiant)) {
-            throw new Exception("Étudiant non trouvé");
+        public void ajouter(Etudiant etudiant) {
+            etudiants.add(etudiant);
         }
-    }
 
-    public void modifier(Etudiant etudiant, String matricule, String nom, String prenom, String classe) {
-        etudiant.setMatricule(matricule);
-        etudiant.setNom(nom);
-        etudiant.setPrenom(prenom);
-        etudiant.setClasse(classe);
-    }
-
-    public ArrayList<Etudiant> getTous() {
-        return etudiants;
-    }
-    public Etudiant rechercherParMatricule(String matricule) {
-        for (Etudiant etudiant : etudiants) {
-            if (etudiant.getMatricule().equals(matricule)) {
-                return etudiant;
+        public void supprimer(Etudiant etudiant) throws Exception {
+            if (etudiants.contains(etudiant)) {
+                etudiants.remove(etudiant);
+            }else {
+                throw new Exception("Étudiant non trouvé");
             }
         }
-        return null;
+
+        public void modifier(Etudiant etudiant, String matricule, String nom, String prenom, String classe) {
+            etudiant.setMatricule(matricule);
+            etudiant.setNom(nom);
+            etudiant.setPrenom(prenom);
+            etudiant.setClasse(classe);
+        }
+
+        public ArrayList<Etudiant> getTous() {
+            return etudiants;
+        }
+        public Etudiant rechercherParMatricule(String matricule) {
+            for (Etudiant etudiant : etudiants) {
+                if (etudiant.getMatricule().equals(matricule)) {
+                    return etudiant;
+                }
+            }
+            return null;
+        }
     }
-}
